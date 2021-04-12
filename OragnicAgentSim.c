c3e5ccb4f593614afdddcc6206d6f31a7114ff2d
Olle Ronstad <Olle.Ronstad@gmail.com>
2021-04-12 22:29:33 +0200

    added git ignore and removed the exicutable

--- OragnicAgentSim.c
+++ OragnicAgentSim.c
@@ -5,7 +5,7 @@
 /*Declaring structs*/
 struct Cell
 {
-    int pheromone;
+    int Pheromone;
 };
 struct Agent
 {
@@ -15,19 +15,37 @@ struct Agent
     float RotationSpeed;
 };
 
-struct Cell GetCellAt(int x,int y,struct Cell *Array, int Width){
+struct Cell GetCellAt(int x,int y,struct Cell *Array, int Width, int Height){
+    /* if reqested cell is ouside boundery, return Cell with all valaus at int 0 */
+    if ((x > Width-1 || x < 0)||(y > Height-1 || y < 0))
+    {   
+        struct Cell EmptyCell;
+        EmptyCell.Pheromone = 0;
+        return EmptyCell;
+    }
+    
+
     return Array[y*Width + x];
 };
 
-
+int SetCellValues(struct Cell CellValues, int x,int y,struct Cell *Array, int Width, int Height){
+    /* if reqested cell is ouside boundery, raise error */
+    if ((x > Width-1 || x < 0)||(y > Height-1 || y < 0))
+    {   
+        printf("Requsted invalid Cell, at posotion x%i, y%i", x ,y );
+        return 0;
+    }
+    Array[y*Width + x] = CellValues;
+    return 0;
+}
 
 int main() {
     
 
-    /*Declaring variables*/
-    const unsigned int  Width = 1920;
-    const unsigned int Height = 1080;
-    const unsigned int Steps = 250;
+    /* Declaring variables */
+    const int  Width = 1920;
+    const int Height = 1080;
+    const int Steps = 250;
     const int AgentAmount = 10000;
     int Disipate = 5;
     float RotationSpeedMult = 1.0;
@@ -35,8 +53,8 @@ int main() {
     float AgentVelocity = 1.0;
     srand(10U);
 
-    /*initialize "Array" of struct Cell
-    useing malloc to avoid stack overflowing instead of "struct Cell CellArray[1920][1080];"
+    /* initialize "Array" of struct Cell
+    useing malloc instead of "struct Cell CellArray[1920][1080];" to avoid stack overflowing
     format of "Array", number refers to index
     0  1  2  3
     4  5  6  7
@@ -44,13 +62,13 @@ int main() {
     */
     struct Cell* CellArray = malloc(Width*Height*sizeof(*CellArray));
 
-    /*initialize of array of agent's
+    /* initialize of array of agent's
     struct Agent AgentArray[10000];
     useing malloc to avoid stack overflowing,
     */
     struct Agent* AgentArray = malloc(AgentAmount*sizeof(*AgentArray));
 
-    /* Positining Agnt in the middle of the CellArray with random rotation*/ 
+    /* Positining Agnt in the middle of the CellArray with random rotation */ 
     for (int i = 0; i < AgentAmount; i++)
     {
         AgentArray[i].PositionY = (float)Height / 2.0;
@@ -59,6 +77,13 @@ int main() {
         printf("%f\n",AgentArray[i].Rotation);
     };
     
+    /* "main loop" */
+    for (int i = 0; i < Steps; i++)
+    {
+        for(int x = 0; x < Width*Height; x++){
+
+        };
+    };
 
     return 0;
 }
