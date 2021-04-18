b813df827cf0d459d414097ac9ebc2465533dee4
Olle Ronstad <Olle.Ronstad@gmail.com>
2021-04-18 19:05:17 +0200

    fixed trig issues, changed positioning agents

--- OrganicAgentSim.c
+++ OrganicAgentSim.c
@@ -26,22 +26,19 @@ const int AgentAmount = 10000;
 int Disipate = 1;
 double RotationSpeedMult = 1.0f;
 double RandRotationSpeed = 1.0f;
-double AgentVelocity = 5.0f; 
+double AgentVelocity = 5.0f;
 double SearchOffset = 0.5f;
 double SearchDistans = 20.0f;
-int SearchSize = 2;
+int SearchSize = 1;
 
-
-
-int inttmp;      
+int inttmp;
 struct Cell *tmp;
-int SearchPos0 ;
-int SearchPos1 ;
-int SearchPos2 ;
-
+int SearchPos0;
+int SearchPos1;
+int SearchPos2;
 
 /*
-fuinction: returns the cell att the given position
+fuinction: returns the cell att the given position, if the position is outside retun a struct with all values at 0
 Arg 0(int x): the x position of the requsted cell
 Arg 1(int y): the y position of the requsted cell
 Arg 2(pointer to struct Cell Array): The Array of cells
@@ -66,7 +63,6 @@ struct Cell GetCellAt(int x, int y, struct Cell *Array, int Width, int Height)
 
     return Array[y * Width + x];
 };
-
 
 /*
 fuinction: replace the cell at the position with the given cell
@@ -99,20 +95,22 @@ int SetCellValues(struct Cell CellValues, int x, int y, struct Cell *Array, int 
 fuinction: postions the agent in the middle with random rotation
 Arg 0(pointer to Struct Agent Array): the agents to position
 inplace: Yes
-Todo:
+Todo: position them 
 Update:
 By: Olle Ronstad
 Date: 2021-04-18
 */
 void PositionAgents(struct Agent *AgentArray)
 {
+    double doubleTmp;
     /* Positining Agnt in the middle of the CellArray with random rotation */
     for (int i = 0; i < AgentAmount; i++)
     {
-        AgentArray[i].PositionY = ((double)Height - 1.0) / 2.0;
-        AgentArray[i].PositionX = ((double)Width - 1.0) / 2.0;
-        AgentArray[i].Rotation = ((double)rand() / (double)(RAND_MAX)) * 2.0 * M_PI;
-        AgentArray[i].RotationSpeed = 0.1;
+        doubleTmp = ((double)rand() / (double)(RAND_MAX)) * 2.0 * M_PI;
+        AgentArray[i].PositionY = ((double)Height - 1.0) / 2.0 + sin(doubleTmp)*200;
+        AgentArray[i].PositionX = ((double)Width - 1.0) / 2.0 - cos(doubleTmp)*200;
+        AgentArray[i].Rotation = doubleTmp;
+        AgentArray[i].RotationSpeed = 0;
     };
 }
 
@@ -146,16 +144,7 @@ void UpdateCells(struct Cell **CellArray, struct Cell **TmpCellArray)
         {
             struct Cell BluredCell;
             BluredCell.Pheromone =
-                (GetCellAt(Width_i - 1, Height_i + 1, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i, Height_i + 1, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i + 1, Height_i + 1, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i - 1, Height_i, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i, Height_i, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i + 1, Height_i, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i - 1, Height_i - 1, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i, Height_i - 1, (*CellArray), Width, Height).Pheromone 
-                + GetCellAt(Width_i + 1, Height_i - 1, (*CellArray), Width, Height).Pheromone) 
-                / 9;
+                (GetCellAt(Width_i - 1, Height_i + 1, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i, Height_i + 1, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i + 1, Height_i + 1, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i - 1, Height_i, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i, Height_i, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i + 1, Height_i, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i - 1, Height_i - 1, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i, Height_i - 1, (*CellArray), Width, Height).Pheromone + GetCellAt(Width_i + 1, Height_i - 1, (*CellArray), Width, Height).Pheromone) / 9;
             SetCellValues(BluredCell, Width_i, Height_i, *TmpCellArray, Width, Height);
         };
     };
@@ -176,13 +165,14 @@ Update:
 By: Olle Ronstad
 Date: 2021-04-18
 */
-int SearchArea(int x, int y,int SearchSize, struct Cell *CellArray){
+int SearchArea(int x, int y, int SearchSize, struct Cell *CellArray)
+{
     inttmp = 0;
-    for (int Width_i = x-SearchOffset; Width_i < x+SearchOffset; Width_i++)
+    for (int Width_i = x - SearchOffset; Width_i < x + SearchOffset; Width_i++)
     {
         for (int Height_i = y - SearchOffset; Height_i < y + 1; Height_i++)
         {
-            inttmp = inttmp + GetCellAt(Width_i, Height_i, CellArray,Width,Height).Pheromone;
+            inttmp = inttmp + GetCellAt(Width_i, Height_i, CellArray, Width, Height).Pheromone;
         }
     }
     return inttmp;
@@ -209,10 +199,10 @@ void UpdateAgents(struct Agent *AgentArray, struct Cell *CellArray)
         //adding some random rotation speed to keep the agents from moving in a starigt line
         AgentArray[i].RotationSpeed = ((double)rand() / (double)(RAND_MAX)-0.5) * RandRotationSpeed;
 
-        //get cells att search positions 
-        SearchPos0 = SearchArea((int)(AgentArray[i].PositionX+(cos(AgentArray[i].Rotation + SearchOffset)*SearchDistans)),(int)(AgentArray[i].PositionY-sin(AgentArray[i].Rotation + SearchOffset)*SearchDistans),SearchSize,CellArray);
-        SearchPos1 = SearchArea((int)(AgentArray[i].PositionX+(cos(AgentArray[i].Rotation)*SearchDistans)),(int)(AgentArray[i].PositionY-sin(AgentArray[i].Rotation)*SearchDistans),SearchSize,CellArray);
-        SearchPos2 = SearchArea((int)(AgentArray[i].PositionX+(cos(AgentArray[i].Rotation - SearchOffset)*SearchDistans)),(int)(AgentArray[i].PositionY-sin(AgentArray[i].Rotation - SearchOffset)*SearchDistans),SearchSize,CellArray);
+        //get cells att search positions
+        SearchPos0 = SearchArea((int)(AgentArray[i].PositionX + (cos(AgentArray[i].Rotation + SearchOffset) * SearchDistans)), (int)(AgentArray[i].PositionY - sin(AgentArray[i].Rotation + SearchOffset) * SearchDistans), SearchSize, CellArray);
+        SearchPos1 = SearchArea((int)(AgentArray[i].PositionX + (cos(AgentArray[i].Rotation) * SearchDistans)), (int)(AgentArray[i].PositionY - sin(AgentArray[i].Rotation) * SearchDistans), SearchSize, CellArray);
+        SearchPos2 = SearchArea((int)(AgentArray[i].PositionX + (cos(AgentArray[i].Rotation - SearchOffset) * SearchDistans)), (int)(AgentArray[i].PositionY - sin(AgentArray[i].Rotation - SearchOffset) * SearchDistans), SearchSize, CellArray);
 
         // determine if a trun is necsecary
         if (!(SearchPos1 > SearchPos0) && !(SearchPos1 > SearchPos2))
@@ -228,22 +218,24 @@ void UpdateAgents(struct Agent *AgentArray, struct Cell *CellArray)
             }
         }
         //Move Agent
-        AgentArray[i].PositionX = AgentArray[i].PositionX - sin(AgentArray[i].Rotation) * AgentVelocity;
-        AgentArray[i].PositionY = AgentArray[i].PositionY + cos(AgentArray[i].Rotation) * AgentVelocity;
+        AgentArray[i].PositionX = AgentArray[i].PositionX + cos(AgentArray[i].Rotation) * AgentVelocity;
+        AgentArray[i].PositionY = AgentArray[i].PositionY - sin(AgentArray[i].Rotation) * AgentVelocity;
 
         //if agent is outside boundery
-        if ((AgentArray[i].PositionX > (double)(Width - 1) || AgentArray[i].PositionX < 0.0) || (AgentArray[i].PositionY > (double)(Height - 1) || AgentArray[i].PositionY < 0.0)){
+        if ((AgentArray[i].PositionX > (double)(Width - 1) || AgentArray[i].PositionX < 0.0) || (AgentArray[i].PositionY > (double)(Height - 1) || AgentArray[i].PositionY < 0.0))
+        {
             AgentArray[i].PositionX = AgentArray[i].PositionX + sin(AgentArray[i].Rotation) * AgentVelocity;
             AgentArray[i].PositionY = AgentArray[i].PositionY - cos(AgentArray[i].Rotation) * AgentVelocity;
             AgentArray[i].Rotation = ((double)rand() / (double)(RAND_MAX)) * 2.0 * M_PI;
         }
         struct Cell FullCell;
         FullCell.Pheromone = 255;
-        if (SetCellValues(FullCell, (int)(AgentArray[i].PositionX), (int)(AgentArray[i].PositionY), CellArray, Width, Height)){
-            fprintf(stderr,"Agent number %i\nx %f\ny %f\nrotation %f\nRotSpeed %f\n",i,AgentArray[i].PositionX,AgentArray[i].PositionY,AgentArray[i].Rotation,AgentArray[i].RotationSpeed);
+        if (SetCellValues(FullCell, (int)(AgentArray[i].PositionX), (int)(AgentArray[i].PositionY), CellArray, Width, Height))
+        {
+            fprintf(stderr, "Agent number %i\nx %f\ny %f\nrotation %f\nRotSpeed %f\n", i, AgentArray[i].PositionX, AgentArray[i].PositionY, AgentArray[i].Rotation, AgentArray[i].RotationSpeed);
         }
     };
-    
+
     //printf("Rotation %f\nPosX %f\nPosY %f\n\n",AgentArray[AgentAmount-1].Rotation ,AgentArray[AgentAmount-1].PositionX,AgentArray[AgentAmount-1].PositionY);
 }
 
@@ -255,22 +247,25 @@ Update:
 By: Olle Ronstad
 Date: 2021-04-18
 */
-void SavePng(struct Cell *CellArray, char *Name){
-    unsigned char* image = malloc(Width * Height * 4);
-    for(int y = 0; y < Height; y++) {
-        for(int x = 0; x < Width; x++) {
+void SavePng(struct Cell *CellArray, char *Name)
+{
+    unsigned char *image = malloc(Width * Height * 4);
+    for (int y = 0; y < Height; y++)
+    {
+        for (int x = 0; x < Width; x++)
+        {
             image[4 * Width * y + 4 * x + 0] = 0;
             image[4 * Width * y + 4 * x + 1] = 0;
-            image[4 * Width * y + 4 * x + 2] = GetCellAt(x,y,CellArray,Width,Height).Pheromone;
+            image[4 * Width * y + 4 * x + 2] = GetCellAt(x, y, CellArray, Width, Height).Pheromone;
             image[4 * Width * y + 4 * x + 3] = 255;
         }
     }
-    lodepng_encode32_file(Name,image,Width,Height);
+    lodepng_encode32_file(Name, image, Width, Height);
 }
 
 int main()
 {
-    srand(16U);
+    srand(400U);
 
     /* initialize "Array" of struct Cell
     useing calloc instead of "struct Cell CellArray[1920][1080];" to avoid stack overflowing
@@ -301,14 +296,14 @@ int main()
         UpdateCells(&CellArray, &TmpCellArray);
         printf("Uppdating Agents\n");
         UpdateAgents(AgentArray, CellArray);
-        
-        sprintf(Name,"jpg/%03d.jpg",CurentStep);
+
+        sprintf(Name, "jpg/%03d.jpg", CurentStep);
         printf("Saving image\n");
-        SavePng(CellArray,Name);
+        SavePng(CellArray, Name);
         strcat(Name, "\n");
         printf(Name);
     };
-    
+
     printf("Done");
     free(CellArray);
     free(TmpCellArray);
